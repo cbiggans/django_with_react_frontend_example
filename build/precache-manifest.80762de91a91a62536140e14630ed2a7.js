728b64f52533e7dda44e56cfa29b770c170ca05c
Clarence Biggans <clarencebiggans@gmail.com>
2019-08-28 14:55:31 -0700

    initial commit

---
+++ build/precache-manifest.80762de91a91a62536140e14630ed2a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7adc820ba0c03d9a2e4adec457a5b51b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fc75453b076017ba866",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "68be9632770eac611888",
+    "url": "/static/js/2.2fb007e0.chunk.js"
+  },
+  {
+    "revision": "7fc75453b076017ba866",
+    "url": "/static/js/main.051dba07.chunk.js"
+  },
+  {
+    "revision": "fd4516a517be475de5c3",
+    "url": "/static/js/runtime~main.6c20e4f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
